e73351cb67c17d2bdd2f923127aa685c75fc476c
Blake Saylor <blakesaylor@gmail.com>
2022-08-28 17:08:46 -0600

    Add tests and functionality for Merchants Show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -24,6 +24,26 @@ describe "Merchants API" do
       expect(merchant[:attributes]).to be_an(Hash)
       expect(merchant[:attributes][:name]).to eq Merchant.find(merchant[:id])[:name]
     end
+  end
 
+  it 'can get one Merchant by its ID' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_an(String)
+
+    expect(merchant[:data]).to have_key(:type)
+    expect(merchant[:data][:type]).to be_an(String)
+    expect(merchant[:data][:type]).to eq('merchant')
+
+    expect(merchant[:data]).to have_key(:attributes)
+    expect(merchant[:data][:attributes]).to be_an(Hash)
+    expect(merchant[:data][:attributes][:name]).to eq Merchant.find(merchant[:data][:id])[:name]
   end
 end
