4b0f46a77a344ddce03bcc7ec5f83076ac22f8b8
Blake Saylor <blakesaylor@gmail.com>
2022-08-28 18:05:14 -0600

    Add tests and functionality to create Item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -73,4 +73,25 @@ describe "Items API" do
     expect(item[:data][:attributes][:merchant_id]).to eq Item.find(item[:data][:id])[:merchant_id]
     expect(item[:data][:attributes][:merchant_id]).to be_an(Integer)
   end
+
+  it "can create a new item" do
+    merchant = create(:merchant)
+    item_params = ({
+                    name: Faker::Food.dish,
+                    description: Faker::Food.description,
+                    unit_price: Faker::Number.decimal(r_digits: 2),
+                    merchant_id: merchant.id
+                  })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end
