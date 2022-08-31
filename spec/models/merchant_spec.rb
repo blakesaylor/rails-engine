a4cc6b31ad162cdad044e50af665c1735c662329
Blake Saylor <blakesaylor@gmail.com>
2022-08-31 12:22:53 -0600

    Add tests & pass for Merchant Find One, restructure folders

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -15,7 +15,20 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'class methods' do
-    
+    describe '#find_one' do
+      it 'can find the first instance of a search ' do
+        create_list(:merchant, 5)
+
+        expected_merchant = Merchant.third
+        wrong_merchant = Merchant.fifth
+        wrong_merchant.update(name: expected_merchant.name)
+
+        search_name = expected_merchant.name.upcase
+
+        expect(Merchant.find_one(search_name).id).to eq 3
+        expect(Merchant.find_one(search_name).id).to_not eq 5
+      end
+    end
   end
 
   describe 'instance methods' do
